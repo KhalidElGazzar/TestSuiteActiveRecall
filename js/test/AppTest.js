1646d17384b34417bed4f6930a8b03391d2659f4
Khalid.elgazzar <Khalid.elgazzar@gmail.com>
2019-11-19 19:39:14 +0200

    Added TestSuite and Unit test cases for Pet.

--- js/test/AppTest.js
+++ js/test/AppTest.js
@@ -44,6 +44,48 @@ describe('TS01: Person Test suite', function() {
             })
         }
     )
-
     
+})
+
+
+// create Test suite for Pet class
+describe('TS02: Pet Test suite', function() {
+    // create test cases for Pet class
+
+    // create a new object for pet
+    let nameUnderTest     = 'Meowsy';
+    let speciesUnderTest  = 'cat';
+    let ageUnderTest      = 3.5;
+
+    let Meowsy = new Modules.Pet(nameUnderTest, speciesUnderTest, ageUnderTest);
+
+    describe ('TS02A: Testing the data types', function () {
+            it('TS02.TC01: Unit Test case for Pet - Testing the pet\'s first name is string.', function(){
+                assert.typeOf(Meowsy.name, 'string'); 
+            })
+
+            it('TS02.TC02: Unit Test case for Pet - Testing the pet\'s last name is string.', function(){
+                assert.typeOf(Meowsy.species, 'string'); 
+            })
+
+            it('TS02.TC03: Unit Test case for Pet - Testing the pet\'s age is number.', function(){
+                assert.typeOf(Meowsy.age, 'number'); 
+            })
+        }
+    )
+
+    describe ('TS02B: Testing the values', function () {
+            it('TS02.TC04: Unit Test case for Pet - Testing the pet\'s name.', function(){
+                assert.equal(Meowsy.name, nameUnderTest); 
+            })
+        
+            it('TS02.TC05: Unit Test case for Pet - Testing the pet\'s species.', function(){
+                assert.equal(Meowsy.species, speciesUnderTest); 
+            })
+        
+            it('TS02.TC06: Unit Test case for Pet - Testing the pet\'s age.', function(){
+                assert.equal(Meowsy.age, ageUnderTest); 
+            })
+        }
+    )
 })
